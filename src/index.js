d3a30983f993be59256c6d172b2ba81346b1f79f
Shelly Yusuf <ShellyNYusuf@gmail.com>
2016-11-05 13:53:56 -0700

    fixing merge issues

--- src/index.js
+++ src/index.js
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
-var div = React.DOM.div
-var h1 = React.DOM.h1
 
-//  composite component
-var Title = React.createClass({
-  render() {
-    return (
-      div(null,
-        h1(null, this.props.title)
-      )
-    )
-  }
-})
-
-var HelloWorld = (
-  div( null,
-    React.createElement(Title, {title: 'Hello World Class'})
-  )
-)
-
-ReactDOM.render(HelloWorld, document.getElementById('app'))
-=======
 var div = React.DOM.div;
 var h1 = React.DOM.h1;
 
@@ -42,4 +20,3 @@ var HelloWorld = (
 );
 
 ReactDOM.render(HelloWorld, document.getElementById('app'));
->>>>>>> eaf03de2eac806b83cc8173c1e185ee1a13e5c85
